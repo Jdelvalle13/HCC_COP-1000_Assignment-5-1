10a1335ce21305231df36da6bdcbfcdec50d2b5f
Jdelvalle13 <jdelvalle13@hawkmail.hccfl.edu>
2024-10-13 15:32:01 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+# Part A:
+# a = 1
+a = 1
+# b = 2
+b = 2
+# c = 5
+c = 5
+# while a < c
+while a < c:
+    #   a = a + 1
+    a = a + 1
+    #   b = b + c
+    b = b + c
+# endwhile
+# output a, b, c
+print (a, b, c)
+
+# Part B:
+# d = 4 
+d = 4
+# e = 6
+e = 6
+# f = 7
+f = 7
+# while d > f
+while d > f:
+    # d = d + 1
+    d = d + 1
+    # e = e - 1
+    e = e - 1
+# endwhile
+# output d, e, f
+print (d, e, f)
+# Part C:
+# g = 4
+g = 4
+# h = 6
+h = 6
+# while g < h
+while g < h:
+    # g = g + 1
+    g = g + 1
+# endwhile
+# output g, h
+print (g, h)
+
+# Part D:
+# j = 2
+j = 2
+# k = 5
+k = 5
+# n = 9
+n = 9
+# while j < k
+while j < k:
+    # m = 6
+    m = 6
+    # while m < n
+    while m < n:
+        # output "Goodbye"
+        print ("Goodbye")    
+        # m = m + 1
+        m = m + 1
+    # endwhile
+    # j = j + 1
+    j = j + 1
+# endwhile
+
+# Part E:
+# j = 2
+j = 2
+# k = 5
+k = 5
+# m = 6
+m = 6
+# n = 9
+n = 9
+# while j < k
+while j < k:
+    # while m < n
+    while m < n:
+        # output "Hello"
+        print ("Hello")
+        # m = m + 1
+        m = m + 1
+    # endwhile
+    # j = j + 1
+    j = j + 1
+# endwhile
+
+# Part F
+# 
+# p = 2
+p = 2
+# q = 4
+q = 4
+# while p < q
+while p < q:
+    # output "Adios"
+    print ("Adios")
+    # r =  1
+    r = 1
+    # while r < q
+    while r < q:
+        # output "Adios"
+        print ("Adios")
+        # r = r + 1
+        r = r + 1
+    # endwhile
+    # p = p  + 1
+    p = p + 1
+# endwhile
